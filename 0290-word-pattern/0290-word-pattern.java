326d69dcddcf1a4cb682274ff648060dd123f83a
Anujjj <anujagrawal2343@gmail.com>
2026-09-13 13:06:29 +0530

    Time: 1 ms (83.67%), Space: 42.8 MB (55.61%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character,String> map = new HashMap<>();
+        HashMap<String,Character> reverseMap = new HashMap<>();
+
+        String[] words = s.split(" "); 
+
+        if(pattern.length() != words.length)
+            return false;
+
+        for(int i = 0; i < pattern.length(); i++)
+        {
+            char ch = pattern.charAt(i);
+            
+            if(map.containsKey(ch))
+            {
+                if(!map.get(ch).equals(words[i]))
+                    return false;
+            }
+            else 
+            {
+                if(reverseMap.containsKey(words[i]))
+                    return false;
+            }    
+            map.put(ch, words[i]);
+            reverseMap.put(words[i], ch);
+        }
+        return true;
+    }
+}
